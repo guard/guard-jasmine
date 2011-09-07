39154a118300e93b5e0f9fed15fc4fb6825db5fa
Michael Kessler <michi@netzpiraten.ch>
2011-09-08 00:32:27 +0200

    Add inspector yardoc.

--- lib/guard/jasmine/inspector.rb
+++ lib/guard/jasmine/inspector.rb
@@ -1,8 +1,18 @@
 module Guard
   class Jasmine
+
+    # The inspector verifies of the changed paths are valid
+    # for Guard::Jasmine.
+    #
     module Inspector
       class << self
 
+        # Clean the changed paths and return only valid
+        # Jasmine specs in either JavaScript or CoffeeScript.
+        #
+        # @param [Array<String>] paths the changed paths
+        # @return [Array<String>] the valid spec files
+        #
         def clean(paths)
           paths.uniq!
           paths.compact!
@@ -13,14 +23,28 @@ module Guard
 
         private
 
+        # Tests if the file is valid.
+        #
+        # @param [String] file the file
+        # @return [Boolean] when the file valid
+        #
         def jasmine_spec?(path)
           jasmine_specs.include?(path)
         end
 
+        # Scans the project and keeps a list of all
+        # JavaScript and CoffeeScript files in the `spec`
+        # directory.
+        #
+        # @see #clear_jasmine_specs
+        # @return [Array<String>] the valid files
+        #
         def jasmine_specs
           @jasmine_specs ||= Dir.glob('spec/**/*_spec.{js,js.coffee}')
         end
 
+        # Clears the list of Jasmine specs in this project.
+        #
         def clear_jasmine_specs
           @jasmine_specs = nil
         end
