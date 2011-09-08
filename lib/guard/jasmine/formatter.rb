74613eb233618b4ab9bf552fbbc20ef56b4d7288
Michael Kessler <michi@netzpiraten.ch>
2011-09-08 10:28:09 +0200

    Docs and specs for the formatter.

--- lib/guard/jasmine/formatter.rb
+++ lib/guard/jasmine/formatter.rb
@@ -1,32 +1,73 @@
 module Guard
   class Jasmine
+
+    # The Guard::Jasmine formatter collects console and
+    # system notification methods and enhances them with
+    # some color information.
+    #
     module Formatter
       class << self
 
+        # Print an info message to the console.
+        #
+        # @param [String] message the message to print
+        # @param [Hash] options the output options
+        # @option options [Boolean] :reset reset the UI
+        #
         def info(message, options = { })
           ::Guard::UI.info(message, options)
         end
 
+        # Print a debug message to the console.
+        #
+        # @param [String] message the message to print
+        # @param [Hash] options the output options
+        # @option options [Boolean] :reset reset the UI
+        #
         def debug(message, options = { })
           ::Guard::UI.debug(message, options)
         end
 
+        # Print a red error message to the console.
+        #
+        # @param [String] message the message to print
+        # @param [Hash] options the output options
+        # @option options [Boolean] :reset reset the UI
+        #
         def error(message, options = { })
           ::Guard::UI.error(color(message, ';31'), options)
         end
 
+        # Print a green success message to the console.
+        #
+        # @param [String] message the message to print
+        # @param [Hash] options the output options
+        # @option options [Boolean] :reset reset the UI
+        #
         def success(message, options = { })
           ::Guard::UI.info(color(message, ';32'), options)
         end
 
+        # Outputs a system notification.
+        #
+        # @param [String] message the message to print
+        # @param [Hash] options the output options
+        # @option options [Symbol, String] :image the image to use, either :failed, :pending or :success, or an image path
+        # @option options [String] :title the title of the system notification
+        #
         def notify(message, options = { })
           ::Guard::Notifier.notify(message, options)
         end
 
         private
 
+        # Print a info message to the console.
+        #
+        # @param [String] test the text to colorize
+        # @param [String] color_code the color code
+        #
         def color(text, color_code)
-          ::Guard::UI.send(:color_enabled?) ? "\e[0#{color_code}m#{text}\e[0m" : text
+          ::Guard::UI.send(:color_enabled?) ? "\e[0#{ color_code }m#{ text }\e[0m" : text
         end
 
       end
---
+++ spec/guard/jasmine/formatter_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe Guard::Jasmine::Formatter do
+
+  let(:formatter) { Guard::Jasmine::Formatter }
+  let(:ui) { Guard::UI }
+  let(:notifier) { Guard::Notifier }
+
+  describe '.info' do
+    it 'shows an info message' do
+      ui.should_receive(:info).with('Info message', { :reset => true })
+      formatter.info('Info message', { :reset => true })
+    end
+  end
+
+  describe '.debug' do
+    it 'shows a debug message' do
+      ui.should_receive(:debug).with('Debug message', { :reset => true })
+      formatter.debug('Debug message', { :reset => true })
+    end
+  end
+
+  describe '.error' do
+    it 'shows a colorized error message' do
+      ui.should_receive(:error).with("\e[0;31mError message\e[0m", { :reset => true })
+      formatter.error('Error message', { :reset => true })
+    end
+  end
+
+  describe '.success' do
+    it 'shows a colorized success message' do
+      ui.should_receive(:info).with("\e[0;32mSuccess message\e[0m", { :reset => true })
+      formatter.success('Success message', { :reset => true })
+    end
+  end
+
+  describe '.notify' do
+    it 'shows an info message' do
+      notifier.should_receive(:notify).with('Notify message', { :image => :failed })
+      formatter.notify('Notify message', { :image => :failed })
+    end
+  end
+end
