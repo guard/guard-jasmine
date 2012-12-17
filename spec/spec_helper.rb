207916b72776b322ec050ba15474298f0320b9af
Michael Kessler <michi@netzpiraten.ch>
2012-12-17 14:35:42 +0100

    Stub Guard::UI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,13 @@ RSpec.configure do |config|
   config.before(:each) do
     ENV["GUARD_ENV"] = 'test'
     @project_path    = Pathname.new(File.expand_path('../../', __FILE__))
+    
+    Guard::UI.stub(:info)
+    Guard::UI.stub(:debug)
+    Guard::UI.stub(:error)
+    Guard::UI.stub(:success)
+    Guard::UI.stub(:warning)
+    Guard::UI.stub(:notify)
   end
 
   config.after(:each) do
