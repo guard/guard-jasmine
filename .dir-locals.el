efd9b501f2bd65ea41b51df324313465f2556927
Nathan Stitt <nathan@stitt.org>
2014-11-17 20:11:29 -0600

    Styles config for Emacs (the one true editor)

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((enh-ruby-indent-level . 2)
+         (js-indent-level . 2)
+         (js2-basic-offset . 2)
+         (prelude-clean-whitespace-on-save . nil)
+         (fill-column . 120))))
